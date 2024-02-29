9c3c423889d7ed49b78a835218425eb78d5314fb
German Palacios <ger.rodri09@gmail.com>
2024-02-29 17:36:27 -0300

    Actualización de Comentarios de OP
    Actualización de comentarios de funciones de OP

--- proveedores/views.py
+++ proveedores/views.py
@@ -5,8 +5,10 @@ from django.views import View
 
 from num2words import num2words
 
+
 from proveedores.models import CPOPAGO
 from sistema.functions import generate_pdf
+
 
 
 
@@ -30,31 +32,10 @@ class op_pagadas(View):
 
     def get_context_data(self, **kwargs):
         cuit = 30718402235
-        
+
         # ARMAR PRIMER OBJETO CON NRO DE CUIL
         pagadas = CPOPAGO.objects.filter(cbencui=cuit)
-        # FILTRAR POR EJERCICIO 
-        if self.request.POST.get('ejer'):
-            pagadas = pagadas.filter(cpopanio__icontains=self.request.POST.get('ejer'))
-        # FILTRAR POR JURISDICCION 
-        if self.request.POST.get('jur'):
-            pagadas = pagadas.filter(cjurcod__icontains=self.request.POST.get('jur'))    
-        # FILTRAR POR UNIDAD DE JURISDICCION 
-        if self.request.POST.get('udo'):
-            pagadas = pagadas.filter(crepudo__icontains=self.request.POST.get('udo'))   
-        # FILTRAR POR UNIDAD NUMERO DE OP 
-        if self.request.POST.get('nro_op'):
-            pagadas = pagadas.filter(copanro__icontains=self.request.POST.get('nro_op'))   
-        # FILTRAR POR FECHA DESDE
-        if self.request.POST.get('desde'):
-            pagadas = pagadas.filter(cpopfpg__gte=self.request.POST.get('desde'))
-        # FILTRAR POR FECHA HASTA
-        if self.request.POST.get('hasta'):
-            pagadas = pagadas.filter(cpopfpg__lte=self.request.POST.get('hasta'))
-        # FILTRAR POR TIPOS DE CHEQUES
-        if self.request.POST.get('cheque'):
-            pagadas = pagadas.filter(cpoptip__lte=self.request.POST.get('cheque'))
-            
+
         context = {
             'titulo': "OP Pagadas",
             'pagadas': pagadas,
@@ -67,6 +48,9 @@ class op_pagadas(View):
     def post(self, request, *args, **kwargs):
         return render(request, self.template_name, self.get_context_data())
 
+####################################################################################################
+############################### VISTA DE DETALLE DE ORDEN DE COMPRA ################################
+####################################################################################################
 
 class op_pagadas_detalle(View):
     template_name = 'proveedores/op_pagadas_detalle.html'
@@ -85,6 +69,9 @@ class op_pagadas_detalle(View):
         data['html_form'] = render_to_string(self.template_name, self.get_context_data(), request=request)
         return JsonResponse(data)
 
+####################################################################################################
+##################################### VISTA DE COMPROBANTE DE PAGO #################################
+####################################################################################################
 
 class op_pagadas_comprobante(View):
     template_name = 'proveedores/op_pagadas_comprobante.html'
@@ -103,6 +90,9 @@ class op_pagadas_comprobante(View):
         data['html_form'] = render_to_string(self.template_name, self.get_context_data(), request=request)
         return JsonResponse(data)
 
+####################################################################################################
+#################################### IMPRESOR DE OP ################################################
+####################################################################################################
 class op_pagadas_imprimir(View):
     template_name = 'proveedores/op_pagadas_pdf.html'
 
@@ -122,7 +112,9 @@ class op_pagadas_imprimir(View):
         
         return generate_pdf(request, self.template_name, context)
 
-
+####################################################################################################
+############### FUNCION PARA CONVERTIR NUMEROS DECIMALES A TEXTO (se usa en importe) ###############
+####################################################################################################
 def decimal_a_texto(numero):
     parte_entera = int(numero)
     parte_decimal = int((numero - parte_entera) * 100)  # Multiplica por 100 y convierte a entero
@@ -134,34 +126,37 @@ def decimal_a_texto(numero):
     else:
         return texto_entero
 
+####################################################################################################
+################ FUNCION PARA TRAER DATOS DE LA TABLA CON FILTROS DE BUSQUEDA ######################
+####################################################################################################
+
 def op_pagadas_ajax(request):
-    ejer = request.GET.get('ejer_ajax', None)
     cuit = 30718402235
 
     # ARMAR PRIMER OBJETO CON NRO DE CUIL
     pagadas = CPOPAGO.objects.filter(cbencui=cuit)
 
     # FILTRAR POR EJERCICIO
-    if request.GET.get('ejer_ajax'):
-        pagadas = pagadas.filter(cpopanio__icontains=request.GET.get('ejer_ajax'))
+    if request.POST.get('ejer_ajax'):
+        pagadas = pagadas.filter(cpopanio__icontains=request.POST.get('ejer_ajax'))
     # FILTRAR POR JURISDICCION
-    if request.GET.get('jur_ajax'):
-        pagadas = pagadas.filter(cjurcod__icontains=request.GET.get('jur_ajax'))
+    if request.POST.get('jur_ajax'):
+        pagadas = pagadas.filter(cjurcod__icontains=request.POST.get('jur_ajax'))
         # FILTRAR POR UNIDAD DE JURISDICCION
-    if request.GET.get('udo_ajax'):
-        pagadas = pagadas.filter(crepudo__icontains=request.GET.get('udo_ajax'))
+    if request.POST.get('udo_ajax'):
+        pagadas = pagadas.filter(crepudo__icontains=request.POST.get('udo_ajax'))
         # FILTRAR POR UNIDAD NUMERO DE OP
-    if request.GET.get('nro_op_ajax'):
-        pagadas = pagadas.filter(copanro__icontains=request.GET.get('nro_op_ajax'))
+    if request.POST.get('nro_op_ajax'):
+        pagadas = pagadas.filter(copanro__icontains=request.POST.get('nro_op_ajax'))
         # FILTRAR POR FECHA DESDE
-    if request.GET.get('desde_ajax'):
-        pagadas = pagadas.filter(cpopfpg__gte=request.GET.get('desde_ajax'))
+    if request.POST.get('desde_ajax'):
+        pagadas = pagadas.filter(cpopfpg__gte=request.POST.get('desde_ajax'))
     # FILTRAR POR FECHA HASTA
-    if request.GET.get('hasta_ajax'):
-        pagadas = pagadas.filter(cpopfpg__lte=request.GET.get('hasta_ajax'))
+    if request.POST.get('hasta_ajax'):
+        pagadas = pagadas.filter(cpopfpg__lte=request.POST.get('hasta_ajax'))
     # FILTRAR POR TIPOS DE CHEQUES
-    if request.GET.get('cheque_ajax'):
-        pagadas = pagadas.filter(cpoptip__lte=request.GET.get('cheque_ajax'))
+    if request.POST.get('cheque_ajax'):
+        pagadas = pagadas.filter(cpoptip__lte=request.POST.get('cheque_ajax'))
 
     data = list(pagadas.values())
     return JsonResponse(data, safe=False)
