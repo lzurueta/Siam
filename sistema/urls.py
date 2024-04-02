b0b2871a0a141c237d4db252d0de0cc400aeb053
German Palacios <ger.rodri09@gmail.com>
2024-04-02 00:37:39 -0300

    Actualización de archivos urls
    Se quitaron urls sin uso.

--- sistema/urls.py
+++ sistema/urls.py
@@ -6,11 +6,11 @@ from . import views
 urlpatterns = [
     path('sistemaHome', login_required(views.SistemaHome.as_view()), name="sistemaHome"),
     path('perfil', login_required(views.ProfileView.as_view()), name="userProfile"),
-    path('abmAlta/<str:app_name>/<str:model_name>/<str:form_name>', login_required(views.abmAlta.as_view()), name="abmAlta"),
-    path('abmEliminar/<str:app_name>/<str:model_name>/<str:form_name>', login_required(views.abmEliminar.as_view()), name="abmEliminar"),
+    # path('abmAlta/<str:app_name>/<str:model_name>/<str:form_name>', login_required(views.abmAlta.as_view()), name="abmAlta"),
+    # path('abmEliminar/<str:app_name>/<str:model_name>/<str:form_name>', login_required(views.abmEliminar.as_view()), name="abmEliminar"),
     path('logout_view', login_required(views.logout_view.as_view()), name="logout_view"),
     path('registrarse', views.registerUser, name="registrarUsuario"),
-    path('recuperarConstrasena', views.recuperarConstrasena, name="recuperarConstrasena"),
-    path('', views.home.as_view(), name="home"),
+    # path('recuperarConstrasena', views.recuperarConstrasena, name="recuperarConstrasena"),
+    path('', login_required(views.home.as_view()), name="home"),
 ]
 
