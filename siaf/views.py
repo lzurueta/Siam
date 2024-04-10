0421be91d1343938c541353a21a0f46be5671a20
German Palacios <ger.rodri09@gmail.com>
2024-04-10 18:02:21 -0300

    Cambio de Link / Impresor declaración Jurada
    Se actualizo el link para reimprimir declaración en mail de alta a base 64.

--- siaf/views.py
+++ siaf/views.py
@@ -2,14 +2,17 @@ from sistema.models import Profile
 from sistema.functions import generate_pdf
 from django.contrib.auth.models import User
 
+import base64
 def enviarDeclaracionJurada(request):
 
     template_name = 'registration/declaracion_jurada_pdf.html'
 
-    user = User.objects.get(id=request.GET.get('a'))
+    id = str(request.GET.get('a'))
+    encode = base64.b64decode(id.encode('utf-8'))
+    a = encode.decode('utf-8')
+
+    user = User.objects.get(id=a)
     profile = Profile.objects.get(user=user)
-
-
 
     context = {
         'profile': profile,
